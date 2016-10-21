c38a311cb3858365dde60322f377bf8b2fb944e5
ahmadtout <ahmadtout@gmail.com>
2016-10-21 20:59:21 +0300

    index page
    First

---
+++ Server side/index.php
@@ -0,0 +1,28 @@
+<?php require_once('template_header.php'); ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("top_left_menu.php");?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header" align="center"><?php echo $_SESSION['MM_AppTitle'] ?></h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+           <!-- -----------------------------------Start------------------------------------------------------------>
+           <div class="row">
+                
+                
+                
+                
+            </div>
+            <!-- -------------------------- End --------------------------------------------------------------------->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+<?php require_once('template_footer.php'); ?>
