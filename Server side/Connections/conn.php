f83510b0759507667e2fdb814d6a7815247d15df
ahmadtout <ahmadtout@gmail.com>
2016-10-21 22:22:58 +0300

    connection

---
+++ Server side/Connections/conn.php
@@ -0,0 +1,13 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_conn = "localhost";
+$database_conn = "sira";
+$username_conn = "root";//user_panel
+$password_conn = "";//h=?EKiOEXs(T
+$conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
+mysql_query("set character_set_server='utf8'");
+mysql_query("set names 'utf8'");
+mysql_set_charset('utf8');
+?>
