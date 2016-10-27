a178265234cbdb3e1d9cb4dae448aa3c34859e61
ahmadtout <ahmadtout@gmail.com>
2016-10-27 20:24:44 +0300

    other books

---
+++ Server side/otherbooks.php
@@ -0,0 +1,19 @@
+<?php
+header('Access-Control-Allow-Origin: *'); ?>
+<div>
+ <div>
+  <h3 align="center" style="margin-top:20px;">ترقبوا مزيدا من الكتب .. قريباً </h3>
+ </div>
+ <div style="text-align:right;width: 100%;">
+    <div class="col-xs-12  nopadding">
+      <h2 style="    direction: rtl; padding: 15px;margin: 0;">موقع الإبداع الفكري:</h2>
+      <br />
+     
+     
+     <a href="#" onclick="window.open('http://ebdaafekry.com', '_system');">
+      <img src="img\logo3.jpg" alt="1" width="100%" >
+      </a>
+    </div>
+    <div class="clearfix"></div>
+  </div>
+</div>
