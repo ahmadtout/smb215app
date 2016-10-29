9184aef22bac611d190e603b800272e851d0a6a2
ahmadtout <ahmadtout@gmail.com>
2016-10-29 19:51:55 +0300

    gp audio

--- Server side/gpaudio.php
+++ Server side/gpaudio.php
@@ -37,6 +37,16 @@ $rs_audio = mysql_query($query_rs_audio, $conn) or die(mysql_error());
 $row_rs_audio = mysql_fetch_assoc($rs_audio);
 $totalRows_rs_audio = mysql_num_rows($rs_audio);
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>  
+
+
 
 <div class="col-sm-12 nopadding  demo-container" id="jp_container"  >
  
@@ -162,3 +172,5 @@ $(document).ready(function() {
 <?php
 mysql_free_result($rs_audio);
 ?>
+</body>
+</html>
