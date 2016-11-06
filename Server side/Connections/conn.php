f694a757a22ff4d7ea9bb4a3f4ecf0bbdec6beb0
ahmadtout <ahmadtout@gmail.com>
2016-11-06 17:23:19 +0200

    connection edit

--- Server side/Connections/conn.php
+++ Server side/Connections/conn.php
@@ -3,9 +3,9 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_conn = "localhost";
-$database_conn = "bookServer";
-$username_conn = "root";//user_panel
-$password_conn = "";//h=?EKiOEXs(T
+$database_conn = "u932323222_bookS";
+$username_conn = "u932323222_ahmad";//user_panel
+$password_conn = "Com123";//h=?EKiOEXs(T
 $conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_query("set character_set_server='utf8'");
 mysql_query("set names 'utf8'");
